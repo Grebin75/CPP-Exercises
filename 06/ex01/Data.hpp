76c98c316961ae30d959d23e58e44a1e7666938f
Henrique Grebin <henrique.grebin@gmail.com>
2023-10-11 15:29:12 +0100

    Module 6 ex01

---
+++ 06/ex01/Data.hpp
@@ -0,0 +1,17 @@
+#ifndef DATA_HPP
+#define DATA_HPP
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+
+typedef struct s_Data Data;
+
+struct s_Data
+{
+	std::string	text;
+	int			val;
+};
+
+#endif
---
+++ 06/ex01/Serializer.hpp
@@ -0,0 +1,19 @@
+#ifndef SERIALIZER_HPP
+#define SERIALIZER_HPP
+
+#include "Data.hpp"
+
+class Serializer{
+	private:
+		Serializer();
+		Serializer(const Serializer& copy);
+	public:
+		Serializer& operator=(const Serializer& copy);
+		~Serializer();
+
+		static uintptr_t serialize(Data* ptr);
+		static Data* deserialize(uintptr_t raw);
+};
+
+
+#endif
